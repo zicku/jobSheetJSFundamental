e607b4b44ad8cf58f3be6788cba35c22aed5cf3a
Zicku <2108096004@student.walisongo.ac.id>
2025-08-13 15:40:52 +0700

    update looping

--- javascript-sql/latihan/loopingAktivitas.js
+++ javascript-sql/latihan/loopingAktivitas.js
@@ -1,14 +1,10 @@
-let jumlah = 0;
-
 for (let i = 1; i <= 20; i++) {
     if (i % 2 === 0 && i % 3 === 0) {
         console.log("Mendapatkan Sertifikat");
-        jumlah += i;
     } else if (i % 2 !== 0 && i % 3 === 0) {
         console.log("Mengikuti Uji Kompetensi"); 
     } else if (i % 2 === 0) {
         console.log("Mengikuti Pelatihan"); 
-        jumlah += i;
     } else {
         console.log("Apel Pagi"); 
     }
