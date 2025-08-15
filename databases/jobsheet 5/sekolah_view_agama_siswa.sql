6fa80f2ded7ad85a465b894b3fe32e1b3621fe92
ZickuPerson <118911620+zicku@users.noreply.github.com>
2025-08-15 11:27:30 +0700

    Delete databases directory

--- databases/jobsheet 5/sekolah_view_agama_siswa.sql
+++
@@ -1,8 +0,0 @@
-use sekolah;
-
-create view vw_agama_siswa as 
-select siswa.nis as NIS ,
-siswa.nama as Nama_Siswa,
-agama.nama as Agama_Siswa
-from siswa,agama 
-where siswa.kd_agama = agama.kode;
--- databases/jobsheet2/siswa_Update.sql
+++
@@ -1,3 +0,0 @@
-use sekolah;
-
-update siswa set nama='suneo' where nis='4567891003'
